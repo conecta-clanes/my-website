9133081bc40a35d0921995f80f6ef246dde72daf
Clan de Rovers Anzac-Alcira <clan007voyager@gmail.com>
2025-08-06 10:08:59 -0600

    AZC-007 init
    AZC-007 init

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://conecta-clanes.github.io/my-site/',
+  url: 'https://conecta-clanes.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/pages/conecta-clanes/my-site/',
+  baseUrl: '/conecta-clanes/my-site/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
